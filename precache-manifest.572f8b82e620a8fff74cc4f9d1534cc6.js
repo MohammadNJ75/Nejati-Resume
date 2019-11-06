9c2e35f3c3715437f713c6cd1079aad570a5176e
Mohammad Nejati <49901145+MohammadNJ75@users.noreply.github.com>
2019-11-06 04:21:27 +0330

    Updates

---
+++ precache-manifest.572f8b82e620a8fff74cc4f9d1534cc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a519b6de9adcb7771f502095835c2b",
+    "url": "/Nejati-Resume/index.html"
+  },
+  {
+    "revision": "d367018446e27f29aaf7",
+    "url": "/Nejati-Resume/static/css/main.9c5b7aea.chunk.css"
+  },
+  {
+    "revision": "78aeb2f6ccdc2ede515d",
+    "url": "/Nejati-Resume/static/js/2.8b86cf8e.chunk.js"
+  },
+  {
+    "revision": "d367018446e27f29aaf7",
+    "url": "/Nejati-Resume/static/js/main.eb4624a5.chunk.js"
+  },
+  {
+    "revision": "2d7105537a18003a19ab",
+    "url": "/Nejati-Resume/static/js/runtime-main.6f930bf4.js"
+  }
+]);
